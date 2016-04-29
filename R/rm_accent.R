e44c2aab4d89db702ed0c6588384abcb1cac9db4
Steve Simpson <steventroysimpson@gmail.com>
2016-04-29 16:57:30 -0400

    added more functionality

--- R/rm_accent.R
+++ R/rm_accent.R
@@ -5,5 +5,6 @@
 #' @param x text string / vector
 #' @export
 rm_accent <- function(x) {
+  # http://stackoverflow.com/questions/20495598/replace-accented-characters-in-r-with-non-accented-counterpart-utf-8-encoding
   iconv(x, to='ASCII//TRANSLIT')
 }
